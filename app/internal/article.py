4fb7371af93f90890350b2901033c54d1ea6522c
_ <-@>
2024-02-02 00:53:58 +0300

    init

--- app/internal/article.py
+++ app/internal/article.py
@@ -1,25 +1,29 @@
-from fastapi import FastAPI,APIRouter
+from fastapi import FastAPI, APIRouter
 from pydantic import BaseModel
 import psycopg2
+from datetime import datetime
 
+date = "%s.%s.%s" % (datetime.now().year, datetime.now().month, datetime.now().day)
+app=FastAPI()
 router = APIRouter(prefix="/api/v1")
+
 
 class Article(BaseModel):
     name: str
     article: str
-    date: str
     person_id: int
 
-app=FastAPI(title="Article")
 bd = psycopg2.connect(dbname="postgres", host="localhost", user="postgres", password="postgres", port="5432")
-@app.get("/article")
+
+@router.get("/article")
 async def get_all_articles():
     cursor = bd.cursor()
     cursor.execute("SELECT * FROM article")
     result = cursor.fetchall()
     cursor.close()
     return result
-@app.get("/article/{id}")
+
+@router.get("/article/{id}")
 async def get_article_by_id(person_id:int):
     cursor = bd.cursor()
     cursor.execute("SELECT * FROM article WHERE person_id = %s", (person_id,))
@@ -27,24 +31,24 @@ async def get_article_by_id(person_id:int):
     cursor.close()
     return result
 
-@app.post("/article")
+@router.post("/article")
 async def create_article(data:Article):
     cursor = bd.cursor()
     cursor.execute("INSERT INTO article (name, article, date, person_id) VALUES (%s, %s, %s, %s)",
-                   (data.name, data.article, data.date, data.person_id))
+                   (data.name, data.article, date, data.person_id))
     bd.commit()
     cursor.close()
-    return "Succesfully added new article with person's id %s" % data.person_id
+    return "Succesfully added new article %s from %s with person's id %s at %s " % (data.article, data.name, data.person_id, date)
 
-@app.patch("/article")
+@router.patch("/article")
 async def update_article(id: int, data:Article):
     cursor = bd.cursor()
     cursor.execute("UPDATE article SET name = %s, article = %s, date = %s, person_id = %s WHERE id = %s",
-                   (data.name, data.article,data.date,data.person_id,id))
+                   (data.name, data.article, date, data.person_id,id))
     bd.commit()
     cursor.close()
     return "Successfully updated article with id %s" % id
-@app.delete("/article/{id}")
+@router.delete("/article/{id}")
 async def delete_article(id:int):
     cursor = bd.cursor()
     cursor.execute("DELETE FROM article WHERE id = %s" % id)
--- app/internal/person.py
+++ app/internal/person.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI,APIRouter
+from fastapi import FastAPI, APIRouter
 from pydantic import BaseModel
 import psycopg2
 from app.internal import article
 
+app=FastAPI()
 router = APIRouter(prefix="/api/v1")
+
 
 class User(BaseModel):
     name: str
@@ -11,11 +13,8 @@ class User(BaseModel):
 
 
 bd = article.bd
-app = FastAPI(title="Person")
 
-
-
-@app.get("/person")
+@router.get("/person")
 async def get_all():
     cursor = bd.cursor()
     cursor.execute("SELECT * FROM person")
@@ -23,7 +22,7 @@ async def get_all():
     cursor.close()
     return result
 
-@app.get("/person/{id}")
+@router.get("/person/{id}")
 async def get_person(id: int):
     cursor = bd.cursor()
     cursor.execute("SELECT * FROM person WHERE id = %s", (id,))
@@ -31,7 +30,7 @@ async def get_person(id: int):
     cursor.close()
     return result
 
-@app.post("/person")
+@router.post("/person")
 async def create_person(data:User):
     cursor = bd.cursor()
     cursor.execute("INSERT INTO person (name,password) VALUES (%s,%s)", (data.name, data.password))
@@ -39,7 +38,7 @@ async def create_person(data:User):
     cursor.close()
     return "Person %s created" % data.name
 
-@app.patch("/person")
+@router.patch("/person")
 async def update_person(id:int, data:User):
     cursor = bd.cursor()
     cursor.execute("UPDATE person SET name = %s, password = %s WHERE id = %s", (data.name,data.password,id))
@@ -47,7 +46,7 @@ async def update_person(id:int, data:User):
     cursor.close()
     return "Successfully updated person with id %s" % id
 
-@app.delete("/person{id}")
+@router.delete("/person{id}")
 async def delete_person(id:int):
     cursor = bd.cursor()
     cursor.execute("DELETE FROM person WHERE id = %s" % id)
