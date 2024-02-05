9890c92a3061bfe5027c596b16f4e3971d587d3c
_ <-@>
2024-02-05 17:58:04 +0300

    init

--- postgres/init.sql
+++ postgres/init.sql
@@ -10,14 +10,16 @@ CREATE TABLE IF NOT EXISTS article
     name CHARACTER VARYING(30),
     article CHARACTER VARYING(30),
     date date,
-    person_id INTEGER
+    person_id INTEGER,
+    FOREIGN KEY (id) REFERENCES person (id) ON DELETE CASCADE
 );
 
-CREATE TABLE  IF NOT EXISTS comments
+CREATE TABLE IF NOT EXISTS comments
 (
     id SERIAL PRIMARY KEY,
     article_id INTEGER,
     date date,
     comment text,
-    FOREIGN KEY (article_id) REFERENCES article (id) ON DELETE CASCADE
+    FOREIGN KEY (article_id) REFERENCES article (id) ON DELETE CASCADE,
+    FOREIGN KEY (id) REFERENCES person (id) ON DELETE CASCADE
 );
